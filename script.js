f8f1e86f71e3982905c34b9d5f0b8fa2da3c473a
Hack Reactor Student <student@hackreactor.com>
2017-06-20 14:31:19 -0500

    okay finished

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() { 
-  // write your javascript code below.
+  alert("you have just entered a fortune telling website")
+  var enter = confirm("would you like to have your fortune told?")
+  if (enter == true){
+    var number = prompt("1, 2, 3, or 4")
+    var color = prompt("pick a color yellow or purple")
+    if (color == "purple" && number == 2 ){
+      
+        alert("you will have a happy life." )
+    }else if(color == "purple" && number == 2) { 
+        alert("there is great danger in your future.")
+      
+    }else if(color == "yellow" || number == 3){
+      
+        alert("you will meet a soulmate today.")
+    }else if (number == 4 && color == "yellow"){
+        alert("stay away from the color blue today.")
+    }else{
+      alert("jbkegiojnuvek nxvoygruehrguoknstfygggggggirstviezohtdksjinrgvyu esuialrdfgskjwjgietnsdgsk rgryjkcfsgb u")
+    }
+  }else{
+    alert("maybe next time.")
+  }
   
 });
