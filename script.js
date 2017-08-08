3a66c92f8fdd954e733fc67799903d431b7f5dcb
Hack Reactor Student <student@hackreactor.com>
2017-08-08 16:16:49 -0500

    sydney if-else work

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", function() {
   // write your javascript code below.
-  
+  alert("You've just entered a fortune telling website!");
+  var play = confirm("Would you like to have your fortune told?")
+  console.log(play);
+  if (play == true) {
+  var number = prompt("Pick a number: 1, 2, 3, or 4.")
+}else{
+  alert("BYE!")
+}
+var color = prompt("Pick a color: yellow or purple")
+if (number == 2 && color == "purple") {
+  alert("You will have a happy life!")
+}else if (number == 1 && color == "purple"){
+    alert("There is great danger in your future!")
+}else if (number == 3 || color == "purple"){
+    alert("You will meet your soulmate today!")
+
+
+
 });
