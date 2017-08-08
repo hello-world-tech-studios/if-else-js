379926d5e71148cdc649254449fb28d2a5089eec
Hack Reactor Student <student@hackreactor.com>
2017-08-08 16:14:28 -0500

    davis

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", function() {
   // write your javascript code below.
-  
+  alert("Welcome to a fortune telling website");
+  var play = confirm("Do you want to get your fortune told?")
+  console.log(play)
+  if (play == true){
+    prompt("pick a number between 1-5")
+  }
+  else{
+    alert("Maybe next time")
+  }
+ var color = prompt("yellow or purple")
+ console.log(color)
+  if("play == true; color == purple")
 });
