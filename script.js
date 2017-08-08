121a0fea5516829f9d6c2e92194707c5efa92b72
Hack Reactor Student <student@hackreactor.com>
2017-08-08 16:14:25 -0500

    Justin If-Else Work

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", function() {
   // write your javascript code below.
-  
+  alert("You've just entered a fortune telling website!")
+  var play = confirm("Would you like to have your fortune told?")
+  if (play == true) {
+    var number = prompt("Pick a number, 1, 2, 3, or 4?")
+    var color = prompt("Pick a color, yellow or purple.")
+    if (number == 2 && color == "purple") {
+      alert("You will have a happy life!!!!!")
+    } else if (number == 1 && color == "purple") {
+      alert("There will be great danger in your life!!!!!")
+    } else if (number == 3 || color == "yellow") {
+      alert("You will meet your soulmate today!!!!!")
+    } else if (number == 4 && color == "purple") {
+      alert("Stay away from the clor blue today!!!!!")
+    } else {
+      alert("You didn't follow the rules!!!!!")
+    }
+  } else {
+    alert("Ok, maybe next time.")
+  }
 });
