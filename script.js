c1244db69192cb78697cb6b6d0f534971695a05a
Hack Reactor Student <student@hackreactor.com>
2017-08-08 16:05:28 -0500

    atom's if else

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", function() {
   // write your javascript code below.
-  
+  alert("You've just entered a fortune telling website!");
+  var play = confirm("Would you like to have your fortune told?");
+  if (play == true){
+    var number = prompt("Pick a number: 1, 2, 3, or 4.");
+      if (number == 1){
+        var color1 = prompt("Pick a color: Yellow or Purple.")
+          if (color1 == "Yellow"
+      }else if(number == 2){
+        var number2 = prompt("")
+      }else if(number == 3){
+        var number3 = prompt()
+      }else{
+
+      }
+
+
+  }else{
+    alert("Okay, maybe next time!");
+  }
 });
