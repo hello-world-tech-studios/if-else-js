8798b1045cfc3643d174f32daf4e208d665bce57
Hack Reactor Student <student@hackreactor.com>
2017-08-08 16:15:34 -0500

    max if else work

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() { 
-  // write your javascript code below.
-  
+document.addEventListener("DOMContentLoaded", function() {
+alert("You have entered a fortune telling website!")
+confirm("Are you sure you want to know your fortune?")
+var play = confirm("Are you sure you want to know your fortune?");
+console.log(play);
+if (play == true) {
+  prompt("Pick a number, 1,2,3 or 4")
+
+} else {
+alert("Thanks for visiting, bye")
+}
+var number = prompt("Pick a number, 1,2,3 or 4")
+prompt("Pick a color, yellow or purple.")
+var color = prompt("Pick a color, yellow or purple.")
+if (color == "yellow" && number == 2) {
+  alert("You will die later today.")
+} else if (color == "purple" && number == 4) {
+   alert("You have 2-4 days to live")
+} else if (color == "purple" number == 3) {
+alert("You have a week to live.")
+} else if () {
+
+}
 });
